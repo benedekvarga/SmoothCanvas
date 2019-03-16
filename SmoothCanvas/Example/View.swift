6f72a892b438b07d4cfbfd966ff62dff84731069
Benedek Varga <b.varga@koin.hu>
2019-03-16 16:03:10 +0100

    First push

--- SmoothCanvas/Example/View.swift
+++ SmoothCanvas/Example/View.swift
@@ -11,6 +11,7 @@ import UIKit
 class View: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .white
     }
 
     required init?(coder aDecoder: NSCoder) {
