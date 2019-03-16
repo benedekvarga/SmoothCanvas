6224caac663cb91370d86889ce38b9bff8d18e4c
Benedek Varga <b.varga@koin.hu>
2019-03-16 15:30:42 +0100

    Initial Commit

---
+++ SmoothCanvas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmoothCanvas
+//
+//  Created by Benedek Varga on 2019. 03. 16..
+//  Copyright © 2019. Benedek Varga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
