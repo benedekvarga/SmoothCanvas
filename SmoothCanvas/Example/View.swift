9d03ef7e7982d182bc9549161e93e11b384c305a
Benedek Varga <b.varga@koin.hu>
2019-03-16 16:00:39 +0100

    Sets background to white

--- SmoothCanvas/Example/View.swift
+++ SmoothCanvas/Example/View.swift
@@ -11,7 +11,6 @@ import UIKit
 class View: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .yellow
     }
 
     required init?(coder aDecoder: NSCoder) {
