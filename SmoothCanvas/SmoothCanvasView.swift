f4b1afb116d466521558f07563f7c10bfa777abc
Benedek Varga <b.varga@koin.hu>
2019-04-01 10:59:56 +0200

    Implements smooth line drawing

--- SmoothCanvas/SmoothCanvasView.swift
+++ SmoothCanvas/SmoothCanvasView.swift
@@ -8,77 +8,74 @@
 
 import UIKit
 
-class SmoothCanvasView: UIView {
+open class SmoothCanvasView: UIView {
+    // MARK: - Public properties
+    public var lineWith: CGFloat = 1.4
+    public var lineColor: UIColor  = .black
+    public var isEraser = false
 
-    var points: [CGPoint]?
-    var path: UIBezierPath?
-    var pathLayer: CAShapeLayer!
+    // MARK: - Private properties
+    private var points: [CGPoint]?
+    private var path: UIBezierPath?
+    private var pathLayer: CAShapeLayer!
+    private var drawWidth: CGFloat {
+        return isEraser ? 30 : lineWith
+    }
+    private var drawColor: CGColor {
+        return isEraser ? backgroundColor?.cgColor ?? UIColor.white.cgColor : lineColor.cgColor
+    }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        guard touch.type == .pencil || touch.type == .stylus else { return }
+
         pathLayer = CAShapeLayer()
-        pathLayer.fillColor = UIColor.clear.cgColor
-        pathLayer.strokeColor = UIColor.black.cgColor
-        pathLayer.lineWidth = 3
         pathLayer.lineJoin = CAShapeLayerLineJoin.round
         pathLayer.lineCap = CAShapeLayerLineCap.round
+        pathLayer.fillColor = UIColor.clear.cgColor
+        pathLayer.strokeColor = drawColor
+        pathLayer.lineWidth = drawWidth
         self.layer.addSublayer(pathLayer)
 
-        if let touch = touches.first {
-            points = [touch.location(in: self)]
-        }
+        points = [touch.location(in: self)]
     }
 
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        if let coalescedTouches = event?.coalescedTouches(for: touch) {
-            points? += coalescedTouches.map { $0.location(in: self) }
-        } else {
-            points?.append(touch.location(in: self))
-        }
+        guard touch.type == .pencil || touch.type == .stylus else { return }
+        points?.append(touch.location(in: self))
         guard let points = points else { return }
-        pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points, closed: false).cgPath
-        if let predictedTouches = event?.predictedTouches(for: touch) {
-            let predictedPoints = predictedTouches.map { $0.location(in: self) }
-            pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points + predictedPoints, closed: false).cgPath
-        } else {
-            pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points, closed: false).cgPath
-        }
-    }
-
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let pathPoints = points else { return }
-        pathLayer.path = UIBezierPath.interpolateHermiteFor(points: pathPoints, closed: false).cgPath
-        points?.removeAll()
+        pathLayer.path = points.interpolateHermiteFor().cgPath
     }
 }
 
 
-extension UIBezierPath {
-    static func interpolateHermiteFor(points: [CGPoint], closed: Bool = false) -> UIBezierPath {
-        guard points.count > 1 else { return UIBezierPath() }
+private extension Array where Iterator.Element == CGPoint {
+    func interpolateHermiteFor(closed: Bool = false) -> UIBezierPath {
+        guard self.count > 1 else { return UIBezierPath() }
 
-        if points.count == 2 {
+        if self.count == 2 {
             let bezierPath = UIBezierPath()
-            bezierPath.move(to: points[0])
-            bezierPath.addLine(to: points[1])
+            bezierPath.move(to: self[0])
+            bezierPath.addLine(to: self[1])
             return bezierPath
         }
 
-        let nCurves = closed ? points.count : points.count - 1
+        let nCurves = closed ? self.count : self.count - 1
 
         let path = UIBezierPath()
         for i in 0..<nCurves {
-            var curPt = points[i]
+            var curPt = self[i]
             var prevPt: CGPoint, nextPt: CGPoint, endPt: CGPoint
             if i == 0 {
                 path.move(to: curPt)
             }
 
-            var nexti = (i+1)%points.count
-            var previ = (i-1 < 0 ? points.count-1 : i-1)
+            var nexti = (i+1)%self.count
+            var previ = (i-1 < 0 ? self.count-1 : i-1)
 
-            prevPt = points[previ]
-            nextPt = points[nexti]
+            prevPt = self[previ]
+            nextPt = self[nexti]
             endPt = nextPt
 
             var mx: CGFloat
@@ -98,13 +95,13 @@ extension UIBezierPath {
             ctrlPt1.x = curPt.x + mx / CGFloat(3.0)
             ctrlPt1.y = curPt.y + my / CGFloat(3.0)
 
-            curPt = points[nexti]
+            curPt = self[nexti]
 
-            nexti = (nexti + 1) % points.count
+            nexti = (nexti + 1) % self.count
             previ = i;
 
-            prevPt = points[previ]
-            nextPt = points[nexti]
+            prevPt = self[previ]
+            nextPt = self[nexti]
 
             if closed || i < nCurves-1 {
                 mx  = (nextPt.x - curPt.x) * CGFloat(0.5)
